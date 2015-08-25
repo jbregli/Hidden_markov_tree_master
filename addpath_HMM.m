9115e3411f5aa4b96bf93dda53811af3e41bd2b1
jbRegli <jeanbaptiste.regli@gmail.com>
2015-08-25 10:37:27 +0100

    Cleaner addpath

--- addpath_HMM.m
+++ addpath_HMM.m
@@ -2,7 +2,12 @@ path_to_hmt = fileparts(mfilename('fullpath'));
 
 addpath(genpath('./'));
 
-addpath(fullfile(path_to_hmt, '../../scatnet-master'));
-addpath_scatnet;
+path_to_scatnet = fullfile(path_to_hmt, '../../scatnet-master');
+
+if exist(path_to_scatnet,'dir')
+    addpath(fullfile(path_to_hmt, '../../scatnet-master'));
+    addpath_scatnet;
+end
 
 clear path_to_hmt;
+clear path_to_scatnet;
